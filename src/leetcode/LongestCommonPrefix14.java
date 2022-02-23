0b3478b94dfd538e6166123b867eac6635bb107b
yaraphat <yasiraraphat@gmail.com>
2022-02-23 06:44:42 +0600

    untrack Main.java

--- src/leetcode/LongestCommonPrefix14.java
+++ src/leetcode/LongestCommonPrefix14.java
@@ -5,19 +5,19 @@ public class LongestCommonPrefix14 {
 
         if (strs.length == 1) return strs[0];
 
-        int indexOfLongestPrefixWord, prefixlength, outerLimit, innerLimit;
+        int index, prefixlength, outerLimit, innerLimit;
         outerLimit = strs.length - 1;
         prefixlength = strs[0].length();
 
-        for (indexOfLongestPrefixWord = 0; indexOfLongestPrefixWord < outerLimit; indexOfLongestPrefixWord++) {
-            innerLimit = Math.min(prefixlength, strs[indexOfLongestPrefixWord + 1].length());
+        for (index = 0; index < outerLimit; index++) {
+            innerLimit = Math.min(prefixlength, strs[index + 1].length());
             for (prefixlength = 0; prefixlength < innerLimit; prefixlength++) {
-                if (strs[indexOfLongestPrefixWord].charAt(prefixlength) != strs[indexOfLongestPrefixWord + 1].charAt(prefixlength)) {
+                if (strs[index].charAt(prefixlength) != strs[index + 1].charAt(prefixlength)) {
                     break;
                 }
             }
             if (prefixlength < 1) return "";
         }
-        return prefixlength > 0 ? strs[indexOfLongestPrefixWord].substring(0, prefixlength) : "";
+        return prefixlength > 0 ? strs[index].substring(0, prefixlength) : "";
     }
 }
