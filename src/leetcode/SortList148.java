369f4f1605d8bcdef6cbde3d76cb3e55162d2d5f
yaraphat <yasiraraphat@gmail.com>
2022-02-24 23:55:59 +0600

    148. Sort List

---
+++ src/leetcode/SortList148.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class SortList148 {
+    public static ListNode sortList(ListNode head) {
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
