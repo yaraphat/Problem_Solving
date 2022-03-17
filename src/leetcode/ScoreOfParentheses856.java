ea3ff322341b2b0945a8dc459d688b8ad4acc8be
yaraphat <yasiraraphat@gmail.com>
2022-03-18 01:12:02 +0600

    856. Score of Parentheses

--- src/leetcode/ScoreOfParentheses856.java
+++ src/leetcode/ScoreOfParentheses856.java
@@ -1,4 +1,31 @@
 package leetcode;
 
 public class ScoreOfParentheses856 {
+    public static int scoreOfParentheses(String s) {
+        int score = 0;
+        int carry = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                if (s.charAt(i + 1) == ')') {
+                    score += carry;
+                }
+                carry *= 2;
+            }else{
+                carry /= 2;
+            }
+        }
+
+        return score;
+
+    }
+
+    public static void test(String s) {
+        if (s != null) {
+            System.out.println(scoreOfParentheses(s));
+        } else {
+            System.out.println(scoreOfParentheses("((())(()))"));
+        }
+    }
+
+
 }
