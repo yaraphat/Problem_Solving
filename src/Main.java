e3eb927b4f38dcb7c8a4f04a3a5e4876a2dd6e9b
Yasir Araphat <yasir.araphat@cloudframe.com>
2024-04-23 11:44:33 +0600

    Leetcode -> 189. Rotate Array

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import leetcode.LongestCommonPrefix14;
-import leetcode.ProductOfArrayExceptSelf238;
-import leetcode.ScoreOfParentheses856;
-import leetcode.SortList148;
-
-import java.util.*;
-import java.util.function.Function;
+import leetcode.RotateArray189;
 
 public class Main {
     public static void main(String[] args) {
-        long time = new Date().getTime();
-        ProductOfArrayExceptSelf238.test();
-        System.out.println("Total time elapsed = " + (new Date().getTime() - time) + " ms");
+        int[] arr = {1};
+        RotateArray189.rotate(arr, 0);
     }
 
 }
