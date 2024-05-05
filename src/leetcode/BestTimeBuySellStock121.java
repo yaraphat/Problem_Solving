17aea15a167a48140f9f14c26b6b6c7f4915ae75
Yasir Araphat <yasiraraphat@gmail.com>
2024-05-05 11:29:26 +0600

    LeetCode -> 121. Best time to buy and sell stock

---
+++ src/leetcode/BestTimeBuySellStock121.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class BestTimeBuySellStock121 {
+
+    public static void main(String[] args) {
+        int[] prices = new int[] { 7, 1, 5, 3, 6, 4 };
+        int maxProfit = maxProfit(prices);
+        System.out.println(maxProfit);
+    }
+
+    public int maxProfit(int[] prices) {
+        int min = 10000, max = 0, maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            int num = prices[i];
+            if (num < min) {
+                min = max = num;
+                continue;
+            }
+            if (num > max) {
+                max = num;
+                int profit = max - min;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
